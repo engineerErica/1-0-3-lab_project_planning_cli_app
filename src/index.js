a2bf98b71f4fecb6d144caf10c5003322e443efe
engineerErica <ericaponce2002@gmail.com>
2023-05-09 16:03:03 +0000

    Step 5 Complete

--- src/index.js
+++ src/index.js
@@ -3,13 +3,25 @@ const prompt = require('prompt-sync')();
 
 const welcomeAndGetName = () => {
     console.log("Welcome, user!");
-    const name = prompt("What is your name? ")
-    console.log(`Greetings, ${name}. Pleasure to meet you!`)
+    const name = prompt("What is your name? ");
+    console.log(`Greetings, ${name}. Pleasure to meet you!`);
+}
+
+const showOptions = () => {
+    console.log(`
+        Here are your options:
+        1 - play a guessing game
+        2 - words of wisdom
+        3 - cheer you on!
+        Any other key - exit
+    `)
+    const select = prompt("Select 1, 2, or 3: ")
 }
 
 // Primary runner function
 const main = () => {
-    const name = welcomeAndGetName()
+    welcomeAndGetName();
+    showOptions();
 }
 
 main();
