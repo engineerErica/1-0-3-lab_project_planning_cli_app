c1b94cb8549ba89d7eb9f175dd7de58291a1aaa9
engineerErica <ericaponce2002@gmail.com>
2023-05-09 15:36:32 +0000

    Step 3 Complete

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const prompt = require('prompt-sync')();
 
 // Primary runner function
 const main = () => {
-
+    const name = prompt("Hello. What is your name? ")
+    console.log(`Greetings, ${name}.`)
 }
 
 main();
