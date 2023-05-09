7b6551541764ba5cbd3a2ee7b62a89e3e04035d7
engineerErica <ericaponce2002@gmail.com>
2023-05-09 15:48:41 +0000

    Step 4 Complete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 // imports at the top
 const prompt = require('prompt-sync')();
 
+const welcome = () => {
+    console.log("Welcome, user!");
+    const name = prompt("What is your name? ")
+    console.log(`Greetings, ${name}. Pleasure to meet you!`)
+}
+
 // Primary runner function
 const main = () => {
-    const name = prompt("Hello. What is your name? ")
-    console.log(`Greetings, ${name}.`)
+    const name = welcome()
 }
 
 main();
